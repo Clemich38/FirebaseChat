1526975f687bcb5a12a8898286f5b66e418766f8
Clemich38 <clement.letreguilly@gmail.com>
2017-05-11 13:14:22 +0900

    message author added in the bubble

--- app/components/Message.js
+++ app/components/Message.js
@@ -11,10 +11,17 @@ export default class Message extends Component {
       textAlign: (author === me) ? 'right': 'left',
     }
   }
+  
+  authorTextStyle(author, me) {
+    return {
+      color: (author === me) ? '#ddd' : '#777',
+      fontSize: 10,
+      textAlign: (author === me) ? 'right' : 'left',
+    }
+  }
 
   bubbleStyle(author, me) {
     return {
-      // backgroundColor: '#fff',
       borderBottomColor: '#eee',
       borderColor: 'transparent',
       borderWidth: 0,
@@ -25,7 +32,7 @@ export default class Message extends Component {
       marginLeft: (author === me) ? 40 : 10,
       marginRight: (author === me) ? 10 : 40,
       marginBottom: 10,
-      flexDirection: 'row',
+      flexDirection: 'column',
       justifyContent: (author === me) ? 'flex-end' : 'flex-start',
       alignItems: (author === me) ? 'flex-end' : 'flex-start',
       borderRadius: 25,
@@ -37,35 +44,15 @@ export default class Message extends Component {
 
   render() {
     return (
-      // <TouchableHighlight onPress={this.props.onPress}>
       <View style={this.bubbleStyle(this.props.item.author, this.props.me)}>
+        <Text style={this.authorTextStyle(this.props.item.author, this.props.me)}>{this.props.item.author}</Text>
         <Text style={this.textStyle(this.props.item.author, this.props.me)}>{this.props.item.text}</Text>
       </View>
-      // </TouchableHighlight>
     );
   }
 }
 
 
 const styles = StyleSheet.create({
-  li: {
-    backgroundColor: '#fff',
-    borderBottomColor: '#eee',
-    borderColor: 'transparent',
-    borderWidth: 0,
-    paddingLeft: 16,
-    paddingRight: 16,
-    paddingTop: 14,
-    paddingBottom: 16,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    borderRadius: 25,
-  },
-  delete: {
-    color: 'tomato',
-    fontSize: 16,
-  }
+  
 })
